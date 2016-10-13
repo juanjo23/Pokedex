328993cec926e5e77bfae7de6e3fc29708809ba2
Sandoval <juan.jose.sandoval@accenture.com>
2016-10-13 16:53:17 -0500

    testing with protractro in user story 5

--- test/e2e/user.story5.spec.js
+++ test/e2e/user.story5.spec.js
@@ -7,6 +7,9 @@ And the Pokémon I don’t have in my battle box will display a grayed out star
 icon
 */
 
+
+/*All is wrong STAR ICON **/
+
 describe('User Story 5', function(){
 	// Acceptance criteria 1
 	describe('Given the Pokédex main menu is open', function(){
@@ -27,42 +30,47 @@ describe('User Story 5', function(){
 			it('Then a list of 151 (28) Pokémon will appear', function(){
 				expect(element.all(by.repeater('pokemon in pokemons')).count()).toEqual(28);
 			});
+			it('And the Pokémon I have in my battle box will display a colored star icon', function(){
+				var pokemon = element.all(by.css('.battle-gold'));
+				expect(pokemon.count()).toBe(0); 
+			});
+			it('And the Pokémon I don’t have in my battle box will display a grayed out star icon', function(){
+				var caught_pokemon = element.all(by.css('.battle')).get(0); //Bulbasaur
+				expect(caught_pokemon.getAttribute('class')).toMatch('battle');
+			});
 		});
-	});
-});
+	}); 
 
-/*
-			it('And the Pokémon I have caught will display a colored PokéBall icon', function(){
-				var caught_pokemon = element.all(by.css('.pokeball-pink')); 
-				expect(caught_pokemon.count()).toBe(0);   //Empty localstorage
-			});
-			it('And the Pokémon I have not caught will display a grayed out PokéBall icon', function(){
-				var caught_pokemon = element.all(by.css('.pokeball')).get(0); //Bulbasaur
-				expect(caught_pokemon.getAttribute('class')).toMatch('pokeball');
-			});
-		});
-	});
 	// Acceptance criteria 2
-	describe('Given the Pokédex is displaying the list of all Pokémon', function(){
+	describe('Given the Pokédex main menu is open', function(){
 		beforeAll(function(){
-			browser.get('/#/pokemon');  //Default route			
-			browser.waitForAngular();  
+			var btn_menu = element(by.id('btn-menu'));
+			btn_menu.click();			
+			browser.sleep(500);  //Tiempo de espera por la animación de jQuery
 		});
 
-		describe('When I tap on a grayed out PokéBall icon', function(){
+		describe('When I tap on the "Caught Pokemon" button', function(){
 			beforeAll(function(){
-				var pokeball_icon1 = element.all(by.css('.pokeball')).get(0);
-				var pokeball_icon2 = element.all(by.css('.pokeball')).get(1);
-				pokeball_icon1.click();
-				pokeball_icon2.click();
+				var btn_all = element.all(by.css('ul > li')).get(1);
+				btn_all.click();
+				browser.waitForAngular();
 			});
 		
-			it('Then the selected Pokémon will be added to my caught list and And the tapped PokéBall icon will be colored ', function(){
-				var pokeball_icon = element.all(by.css('.pokeball')).get(0);
-				expect(pokeball_icon.getAttribute('class')).toMatch('pokeball-pink');
+			it('Then a list of all the Pokémon I have caugth will appear', function(){
+				expect(element.all(by.repeater('pokemon in caught_pokemon')).count()).toEqual(1);
+			});
+			it('And the Pokémon I have in my battle box will display a colored star icon', function(){
+				var pokemon = element.all(by.css('.battle-gold'));
+				expect(pokemon.count()).toBe(0); 
+
+			});
+			it('And the Pokémon I don’t have in my battle box will display a grayed out star icon', function(){
+				var caught_pokemon = element.all(by.css('.battle')).get(0); //Bulbasaur
+				expect(caught_pokemon.getAttribute('class')).toMatch('battle');
 			});
 		});
-	});
+	}); 
+
 	// Acceptance criteria 3
 	describe('Given the Pokédex is displaying the list of all Pokémon', function(){
 		beforeAll(function(){
@@ -70,94 +78,43 @@ describe('User Story 5', function(){
 			browser.waitForAngular();  
 		});
 
-		describe('When I tap on a colored PokéBall icon', function(){
+		describe('When I tap on a grayed out star icon of a caught Pokémon', function(){
 			beforeAll(function(){
-				var pokeball_icon = element.all(by.css('.pokeball')).get(0);
-				pokeball_icon.click();
+				var star_icon = element.all(by.css('.battle')).get(1);
+				star_icon.click();
 			});
 		
-			it('Then the selected Pokémon will be removed to my caught list And the tapped PokéBall icon will be grayed out', function(){
-				var pokeball_icon = element.all(by.css('.pokeball')).get(0);
-				expect(pokeball_icon.getAttribute('class')).not.toMatch('pokeball-pink');
+			it('Then the selected Pokémon will be added to my battle box and the tapped star icon will be colored', function(){
+				var star_icon = element.all(by.css('.battle')).get(1);
+				expect(star_icon.getAttribute('class')).toMatch('battle-gold');
 			});
 		});
 	});
+/*
 
+//Then a message stating “You haven’t captured this Pokémon yet” will
+//And the tapped star icon will remain grayed out
 	// Acceptance criteria 4
-	describe('Given the Pokédex main menu is open', function(){
+	describe('Given the Pokédex is displaying the list of all Pokémon', function(){
 		beforeAll(function(){
-			browser.get('/#/pokemon');  //Default route
-			var btn_menu = element(by.id('btn-menu'));
-			btn_menu.click();			
-			browser.sleep(500);  //Tiempo de espera por la animación de jQuery
+			browser.get('/#/pokemon');  //Default route			
+			browser.waitForAngular();  
 		});
 
-		describe('When I tap on the "All Pokemon" button', function(){
+		describe('When I tap on a grayed out star icon of a uncaught Pokémon', function(){
 			beforeAll(function(){
-				var btn_all = element.all(by.css('ul > li')).get(1);  //Caught pokemon
-				btn_all.click();
-				browser.waitForAngular();
+				var star_icon = element.all(by.css('.battle')).get(0);
+				star_icon.click();
+				//browser.waitForAngular();  
+				 browser.pause();
 			});
 		
-			it('Then a list of all the Pokémon I’ve caught will appear', function(){
-				expect(element.all(by.repeater('pokemon in caught_pokemon')).count()).toEqual(1);
-			});
-
-			it('And each entry will display the Pokémon basic information', function(){
-				expect(element(by.css('.pokemon-item-image')).isPresent()).toBe(true);
-				expect(element(by.css('.pokemon-item-name')).isPresent()).toBe(true);
-				expect(element(by.css('.pokemon-item-number')).isPresent()).toBe(true);
-				expect(element(by.css('.type')).isPresent()).toBe(true);
-			});
-			it('And all entries will display a colored PokéBall icon', function(){
-				expect(element(by.css('.pokeball')).getAttribute('class')).toMatch('pokeball-pink');
+			it('Then a message stating "You haven’t captured this Pokémon yet" will appear', function(){
+				var alert = element.all(by.css('.alert-toast')).evaluate('alert_message');
+				//expect(alert.isDisplayed()).toBe(true);
+				expect(alert.getText()).toMatch("Hola");
 			});
 		});
 	});
-
-
-	// Acceptance criteria 5
-	describe('Given the Pokédex is displaying the list of caught Pokémon', function(){
-		beforeAll(function(){
-			browser.get('/#/caught');  //Default route			
-			browser.waitForAngular();  
-		});
-
-		describe('When I tap on a colored PokéBall icon', function(){
-			beforeAll(function(){
-				var pokeball_icon = element.all(by.css('.pokeball')).get(0);
-				pokeball_icon.click();
-			});
-			it('Then the selected Pokémon will be removed to my caught list And a button with the label "Undo" will replace the Pokémon entry', function(){
-				var undo_button = element.all(by.css('.btn.undo')).get(0);
-				expect(undo_button.isDisplayed()).toBe(true); 
-				//browser.pause();
-			});
-		});
-	});
-
-	// Acceptance criteria 6
-	describe('Given the Pokédex is displaying the list of caught Pokémon', function(){
-		beforeAll(function(){
-			browser.get('/#/caught');  //Default route			
-			browser.waitForAngular();  
-		});
-
-		describe('And a Pokémon was removed by mistake', function(){
-			beforeAll(function(){
-				var pokeball_icon = element.all(by.css('.pokeball')).get(0);
-				pokeball_icon.click();
-			});
-			it('Then the removed Pokémon will be re-added to the caught list And the re-added Pokémon will replace the "Undo" button', function(){
-				var undo_button = element.all(by.css('.btn.undo')).get(0);
-				undo_button.click();
-				expect(undo_button.isDisplayed()).toBe(false); 
-			});
-		});
-
-	});
-
-});
-
-
 */
+});
